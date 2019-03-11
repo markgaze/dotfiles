34ea52e5c1b5f05c29f3c4946ae997290bdb661f
markgaze <mark.l.gaze@gmail.com>
2019-03-11 17:49:41 +0000

    Change UI Kit menu to use new URLs

--- .i3/uik-menu.sh
+++ .i3/uik-menu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -e "https://ui-kit.emisgroup.uk\nhttps://ui-kit-react.emisgroup.uk\nhttps://ui-kit-angular.emisgroup.uk\nhttps://dev-ui-kit.emisgroup.uk\nhttps://dev-ui-kit-react.emisgroup.uk\nhttps://dev-ui-kit-angular.emisgroup.uk" | rofi -dmenu -p "URL" | { read -r url; xdg-open "$url"; }
+echo -e "https://ui-kit.emisgroup.uk\nhttps://ui-kit-react.emisgroup.uk\nhttps://ui-kit-angular.emisgroup.uk\nhttps://dev.ui-kit.emisgroup.uk\nhttps://dev.ui-kit-react.emisgroup.uk\nhttps://dev.ui-kit-angular.emisgroup.uk" | rofi -dmenu -p "URL" | { read -r url; xdg-open "$url"; }
