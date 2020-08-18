60ab63b50c73280214641c61842506b932d1b407
markgaze <mark.l.gaze@gmail.com>
2020-08-18 21:12:28 +0000

    Symlink the rest of the dotfiles

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+symlink_file() {
+  file="$(pwd)/$1"
+  target="$HOME/$2"
+  echo "Symlinking $file to $target"
+  ln -sf "$file" "$target"
+}
+
 symlink_files() {
   directory=$1
   pattern=$2
   files=$(find "$directory" -name "$pattern")
   for file in $files; do
     filename=$(basename "$file")
-    echo "Symlinking $file to $HOME/$filename"
-    ln -sf "$(pwd)/$file" "$HOME/$filename"
+    symlink_file "$file" "$filename"
   done
 }
 
 echo "Symlinking bash dotfiles..."
 
-symlink_files './bash' '.bash*'
-symlink_files './common' '.*'
+symlink_files 'bash' '.bash*'
+symlink_files 'common' '.*'
+symlink_file 'tmux/.tmux.conf' ".tmux.conf"
+symlink_file 'git/.gitconfig' ".gitconfig"
+symlink_file 'vim/.vimrc' ".vimrc"
